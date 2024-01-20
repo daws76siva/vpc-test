9035984adc2325ab0af63a0bad8ab6a816f221cf
arafath <javahappy77@gmail.com>
2024-01-20 10:55:36 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "roboshop" {
 
-  source = "../terraform-aws-vpc"
-  #source = "git::https://github.com/daws76siva/terraform-aws-vpc.git?ref=main"
+  #source = "../terraform-aws-vpc"
+  source = "git::https://github.com/daws76siva/terraform-aws-vpc.git?ref=main"
   project_name = var.project_name
   environment =  var.environment    
   common_tags = var.common_tags
